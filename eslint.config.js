6700963fcaac6ecd0340e65dc9dc1a9aaac7b88b
nivaldoandrade <nivaldoandradef@gmail.com>
2026-06-28 17:08:05 -0300

    feat(app): Add EditorConfig and ESLint configuration files

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const tsEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const tsEslintParser = require('@typescript-eslint/parser');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      }],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'eol-last': ['error', 'always'],
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      'no-duplicate-imports': 'error',
+      'no-console': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^I[A-Z]',
+            match: true,
+          },
+        },
+      ],
+    },
+  },
+  expoConfig,
+  {
+    ignores: ['dist/*'],
+  },
+  {
+    languageOptions: {
+      parser: tsEslintParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+    },
+  },
+]);
