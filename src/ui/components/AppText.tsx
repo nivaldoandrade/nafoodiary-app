32675c96e1f76bb427c90086961b9a9d199f501a
nivaldoandrade <nivaldoandradef@gmail.com>
2026-06-29 17:31:22 -0300

    feat(app): add AppText component for styled text rendering

---
+++ src/ui/components/AppText.tsx
@@ -0,0 +1,33 @@
+import { theme } from '@/ui/styles/theme';
+import { Text, TextProps } from 'react-native';
+
+interface IAppTextProps extends TextProps {
+  family?: keyof typeof theme.fontFamily;
+  weight?: keyof typeof theme.fontFamily.sans;
+  size?: keyof typeof theme.fontSize;
+  color?: string;
+}
+
+export function AppText({
+  family = 'sans',
+  weight = 'regular',
+  size = 'base',
+  color = theme.colors.black[700],
+  style,
+  ...props
+}: IAppTextProps) {
+
+  return (
+    <Text
+      style={[
+        {
+          fontFamily: theme.fontFamily[family][weight],
+          fontSize: theme.fontSize[size],
+          color,
+        },
+        style,
+      ]}
+      {...props}
+    />
+  );
+}
