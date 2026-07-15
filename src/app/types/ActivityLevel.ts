75fd8e26dd52731655e532f0cfeec05d86fb5f40
nivaldoandrade <nivaldoandradef@gmail.com>
2026-07-15 13:41:12 -0300

    feat(app): add ActivityLevel type and integrate into ActivityLevelStep component for onboarding

---
+++ src/app/types/ActivityLevel.ts
@@ -0,0 +1,7 @@
+export const ActivityLevel = {
+  SENDENTARY: 'SENDENTARY',
+  LIGHT: 'LIGHT',
+  MODERATE: 'MODERATE',
+  HEAVY: 'HEAVY',
+  ATHELETE: 'ATHELETE',
+} as const;
--- src/ui/screens/onboarding/steps/ActivityLevelStep.tsx
+++ src/ui/screens/onboarding/steps/ActivityLevelStep.tsx
@@ -1,31 +1,64 @@
-import { View } from 'react-native';
 
-import { AppText } from '@/ui/components/AppText';
+import { ActivityLevel } from '@/app/types/ActivityLevel';
 import { ButtonApp } from '@/ui/components/Button';
+import { RadioGroup, RadioGroupItem, RadioGroupItemDescription, RadioGroupItemIcon, RadioGroupItemInfo, RadioGroupItemLabel } from '@/ui/components/RadioGroup';
+import { Step, StepContent, StepFooter, StepHeader, StepTitle } from '@/ui/screens/onboarding/components/Step';
 import { useOnboarding } from '@/ui/screens/onboarding/context/useOnboarding';
+import { ArrowRightIcon } from 'lucide-react-native';
 
 export function ActivityLevelStep() {
 
-  const {
-    currentStepIndex,
-    nextStep,
-    previousStep,
-  } = useOnboarding();
+  const { nextStep } = useOnboarding();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <AppText size="4xl">
-        ActivityLevelStep
-      </AppText>
-      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 16 }}>
-        <ButtonApp onPress={previousStep}>
-          Previous
+    <Step>
+      <StepHeader>
+        <StepTitle>Qual seu nível de atividade?</StepTitle>
+      </StepHeader>
+      <StepContent>
+        <RadioGroup>
+          <RadioGroupItem value={ActivityLevel.SENDENTARY}>
+            <RadioGroupItemIcon>😴</RadioGroupItemIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupItemLabel>Sedentário</RadioGroupItemLabel>
+              <RadioGroupItemDescription>Não me exercito</RadioGroupItemDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.LIGHT}>
+            <RadioGroupItemIcon>🙂</RadioGroupItemIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupItemLabel>Leve</RadioGroupItemLabel>
+              <RadioGroupItemDescription>1 a 2 vezes por semana</RadioGroupItemDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.MODERATE}>
+            <RadioGroupItemIcon>🤨</RadioGroupItemIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupItemLabel>Moderado</RadioGroupItemLabel>
+              <RadioGroupItemDescription>3 a 5 vezes por semana</RadioGroupItemDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.HEAVY}>
+            <RadioGroupItemIcon>🫡</RadioGroupItemIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupItemLabel>Pesado</RadioGroupItemLabel>
+              <RadioGroupItemDescription>6 a 7 vezes por semana</RadioGroupItemDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+          <RadioGroupItem value={ActivityLevel.ATHELETE}>
+            <RadioGroupItemIcon>🤯</RadioGroupItemIcon>
+            <RadioGroupItemInfo>
+              <RadioGroupItemLabel>Atleta</RadioGroupItemLabel>
+              <RadioGroupItemDescription>2 vezes por semana</RadioGroupItemDescription>
+            </RadioGroupItemInfo>
+          </RadioGroupItem>
+        </RadioGroup>
+      </StepContent>
+      <StepFooter >
+        <ButtonApp size='icon' onPress={nextStep}>
+          <ArrowRightIcon />
         </ButtonApp>
-        <ButtonApp onPress={nextStep}>
-          Next
-        </ButtonApp>
-        <AppText>{currentStepIndex}</AppText>
-      </View>
-    </View>
+      </StepFooter>
+    </Step>
   );
 }
