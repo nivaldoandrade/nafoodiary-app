cfafc46ab3057e657901ee545e2b36c774d032cc
nivaldoandrade <nivaldoandradef@gmail.com>
2026-09-14 17:22:43 -0300

    chore(app): add expo-auth-session/crypto/web-browser e vars Cognito

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -2,6 +2,8 @@ import * as z from 'zod';
 
 const envSchema = z.object({
   EXPO_PUBLIC_API_URL: z.url(),
+  EXPO_PUBLIC_COGNITO_DOMAIN: z.url(),
+  EXPO_PUBLIC_COGNITO_CLIENT_ID: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
